003319635ab931254a7e507056fef3975d1a41f1
mbouyahy <mourad.bh06@gmail.com>
2024-03-16 01:47:17 +0000

    save

--- srcs/MariaDB/clean.sh
+++ srcs/MariaDB/clean.sh
@@ -1,6 +1,8 @@
 #!bin/bash
 
 
+sudo docker system prune
+
 sudo docker rmi -f $(sudo docker images -q)
 
 sudo docker rm -f mariadb-container
---
+++ srcs/MariaDB/dependencies/script.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+# mysql_install_db --defaults-file=~/.my.cnf
+
+# mysql start
+
+# mysqld --user=mysql --bootstrap
+# mysql status
+
+
+
+if [ ! -d "/var/run/mysqld" ]; then 
+	mkdir -p /var/run/mysqld && chown -R mysql:mysql /var/run/mysqld && chmod 777 /var/run/mysqld
+	chown -R mysql:mysql /var/lib/mysql/
+	chmod -R 755 /var/lib/mysql/
+	chmod -R 755 /etc/mysql/conf.d
+	chmod -R 644 /var/lib/mysql/*
+
+	chown -R mysql:mysql /var/lib/mysql/mysql/
+	chmod -R 755 /var/lib/mysql/mysql/
+	chmod -R 644 /var/lib/mysql/mysql/*
+fi
+
+if [ ! -d "/var/lib/mysql" ]; then
+	mkdir -p /var/lib/mysql
+	chown -R mysql:mysql /var/lib/mysql/
+	chmod -R 755 /var/lib/mysql/
+	chmod -R 644 /var/lib/mysql/*
+
+	chown -R mysql:mysql /var/lib/mysql/mysql/
+	chmod -R 755 /var/lib/mysql/mysql/
+	chmod -R 644 /var/lib/mysql/mysql/*
+fi
+
+# /usr/local/mysql/support-files/mysql.server stop
+
+if [ ! -d "/var/run/mysqld" ]; then
+	mariadb_install_db --user=mysql --datadir=/var/lib/mysql
+	# bin/mysql_install_db --user=mysql \
+    # --basedir=/var/mysql/mysql \
+    # --datadir=/var/mysql/mysql/data
+fi
+	# /usr/bin/mariadb-install-db --defaults-file=~/.my.cnf
+# echo ${WORDPRESS_DATABASE_NAME}
+# mysqld_safe --skip-grant-tables&
+if [ ! -d "/var/lib/mysql/${WORDPRESS_DATABASE_NAME}" ]; then
+	echo "ENTER"
+        cat << EOF > /tmp/temp.sql
+	USE mysql;
+	FLUSH PRIVILEGES;
+	DELETE FROM mysql.user WHERE User='';
+	DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+	ALTER USER 'root'@'localhost' IDENTIFIED BY '${DATABASE_ROOT_PASSWD}';
+	CREATE DATABASE ${WORDPRESS_DATABASE_NAME} CHARACTER SET utf8 COLLATE utf8_general_ci;
+	CREATE USER '${DATABASE_USER}'@'%' IDENTIFIED by '${DATABASE_PASSWD}';
+	GRANT ALL PRIVILEGES ON ${WORDPRESS_DATABASE_NAME}.* TO '${DATABASE_USER}'@'%';
+	FLUSH PRIVILEGES;
+EOF
+        # mysqld --user=mysql --bootstrap < /tmp/temp.sql
+        mariadbd --user=mysql --bootstrap < /tmp/temp.sql
+		# cd /var/run/mysqld
+		# ls -la
+        # rm -f /tmp/temp.sql
+else
+        echo "exist"
+fi
+
+exec "$@"
