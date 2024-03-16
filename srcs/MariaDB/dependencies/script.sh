42b1c1a02328691e92f6da41ca2cda2ff2232515
mbouyahy <mourad.bh06@gmail.com>
2024-03-16 16:04:47 +0000

    save

--- srcs/MariaDB/dependencies/script.sh
+++ srcs/MariaDB/dependencies/script.sh
@@ -9,37 +9,37 @@
 
 
 
-if [ ! -d "/var/run/mysqld" ]; then 
-	mkdir -p /var/run/mysqld && chown -R mysql:mysql /var/run/mysqld && chmod 777 /var/run/mysqld
-	chown -R mysql:mysql /var/lib/mysql/
-	chmod -R 755 /var/lib/mysql/
-	chmod -R 755 /etc/mysql/conf.d
-	chmod -R 644 /var/lib/mysql/*
+# if [ ! -d "/var/run/mysqld" ]; then 
+# 	mkdir -p /var/run/mysqld && chown -R mysql:mysql /var/run/mysqld && chmod 777 /var/run/mysqld
+# 	chown -R mysql:mysql /var/lib/mysql/
+# 	chmod -R 755 /var/lib/mysql/
+# 	chmod -R 755 /etc/mysql/conf.d
+# 	chmod -R 644 /var/lib/mysql/*
 
-	chown -R mysql:mysql /var/lib/mysql/mysql/
-	chmod -R 755 /var/lib/mysql/mysql/
-	chmod -R 644 /var/lib/mysql/mysql/*
-fi
+# 	chown -R mysql:mysql /var/lib/mysql/mysql/
+# 	chmod -R 755 /var/lib/mysql/mysql/
+# 	chmod -R 644 /var/lib/mysql/mysql/*
+# fi
 
-if [ ! -d "/var/lib/mysql" ]; then
-	mkdir -p /var/lib/mysql
-	chown -R mysql:mysql /var/lib/mysql/
-	chmod -R 755 /var/lib/mysql/
-	chmod -R 644 /var/lib/mysql/*
+# if [ ! -d "/var/lib/mysql" ]; then
+# 	mkdir -p /var/lib/mysql
+# 	chown -R mysql:mysql /var/lib/mysql/
+# 	chmod -R 755 /var/lib/mysql/
+# 	chmod -R 644 /var/lib/mysql/*
 
-	chown -R mysql:mysql /var/lib/mysql/mysql/
-	chmod -R 755 /var/lib/mysql/mysql/
-	chmod -R 644 /var/lib/mysql/mysql/*
-fi
+# 	chown -R mysql:mysql /var/lib/mysql/mysql/
+# 	chmod -R 755 /var/lib/mysql/mysql/
+# 	chmod -R 644 /var/lib/mysql/mysql/*
+# fi
 
 # /usr/local/mysql/support-files/mysql.server stop
 
-if [ ! -d "/var/run/mysqld" ]; then
-	mariadb_install_db --user=mysql --datadir=/var/lib/mysql
+# if [ ! -d "/var/run/mysqld" ]; then
+# 	mariadb_install_db --user=mysql --datadir=/var/lib/mysql
 	# bin/mysql_install_db --user=mysql \
     # --basedir=/var/mysql/mysql \
     # --datadir=/var/mysql/mysql/data
-fi
+# fi
 	# /usr/bin/mariadb-install-db --defaults-file=~/.my.cnf
 # echo ${WORDPRESS_DATABASE_NAME}
 # mysqld_safe --skip-grant-tables&
@@ -49,7 +49,7 @@ if [ ! -d "/var/lib/mysql/${WORDPRESS_DATABASE_NAME}" ]; then
 	USE mysql;
 	FLUSH PRIVILEGES;
 	DELETE FROM mysql.user WHERE User='';
-	DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+	DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1', '172.19.0.1');
 	ALTER USER 'root'@'localhost' IDENTIFIED BY '${DATABASE_ROOT_PASSWD}';
 	CREATE DATABASE ${WORDPRESS_DATABASE_NAME} CHARACTER SET utf8 COLLATE utf8_general_ci;
 	CREATE USER '${DATABASE_USER}'@'%' IDENTIFIED by '${DATABASE_PASSWD}';
@@ -57,10 +57,10 @@ if [ ! -d "/var/lib/mysql/${WORDPRESS_DATABASE_NAME}" ]; then
 	FLUSH PRIVILEGES;
 EOF
         # mysqld --user=mysql --bootstrap < /tmp/temp.sql
-        mariadbd --user=mysql --bootstrap < /tmp/temp.sql
+        mysqld --user=mysql --bootstrap < /tmp/temp.sql
 		# cd /var/run/mysqld
 		# ls -la
-        # rm -f /tmp/temp.sql
+        rm -f /tmp/temp.sql
 else
         echo "exist"
 fi
--- srcs/MariaDB/start.sh
+++ srcs/MariaDB/start.sh
@@ -2,3 +2,13 @@
 
 sudo docker build -t mariadb-image .
 sudo docker run -it --name mariadb-container mariadb-image /bin/bash
+
+
+#WORKING 
+# sudo docker build -t mariadb-image .
+# sudo docker run -d -p "3306:3306"  --name mariadb-container mariadb-image
+# sudo docker exec -it mariadb-container /bin/bash
+
+
+#LOGGING
+# mysql -uroot -p1234
